51cc27e3588b22676d05fd3e6bb4250ee9dbe1bd
lktgt15 <64021540+lktgt15@users.noreply.github.com>
2021-01-28 23:14:00 +0900

    Day4 임규태 제출

--- 1주차-완전탐색/Day4/Day4_BOJ1107_임규태.java
+++ 1주차-완전탐색/Day4/Day4_BOJ1107_임규태.java
@@ -9,8 +9,8 @@ public class Main {
 	static Scanner sc = new Scanner(System.in);
 
 	static int n,m,ans;
-	static int LIMIT = 1000000;
-	static boolean[] chk = new boolean[10];
+	static int LIMIT = 1000000; // 100만까지
+	static boolean[] chk = new boolean[10]; // 못누르는거 chk
 	public static void main(String[] args) throws IOException {
 		n = Integer.parseInt(br.readLine());
 		m = Integer.parseInt(br.readLine());
@@ -22,28 +22,28 @@ public class Main {
 			}
 		}
 		
-		ans = Math.abs(n-100);
-		if(!chk[0]) ans = Math.min(ans, 1+n);
+		ans = Math.abs(n-100); // 현재(100)에서 +-로만 접근
+		if(!chk[0]) ans = Math.min(ans, 1+n); // 밑에 함수가 0은 체크를 못해서 따로 체크
 		
-		f(1);
+		f(1); // 1번채널부터 눌러봄
 		System.out.println(ans);
 	}
 	
 	static void f(int curv) {
-		if(curv > LIMIT) return;
+		if(curv > LIMIT) return; // 대충 100만 넘어가면 그냥 100에서 +-로만 가는게 제일 빠름
 		
 		int tmp = curv;
 		int len = 0;
 		boolean flag = true;
-		while(tmp > 0) {
+		while(tmp > 0) { // 길이 계산 + 못누르는거 chk
 			if(chk[tmp%10]) flag = false;
 			tmp /= 10;
 			len++;
 		}
 		
-		if(flag) {
+		if(flag) { // 누를 수 있으면
 			ans = Math.min(ans,len+Math.abs(curv-n));
 		}
-		f(curv+1);
+		f(curv+1); // 다음 채널 체크
 	}
 }
