efb0d86e16dcc0e4284c9551040f58b6904d1726
whyandaeno <77835382+whyandaeno@users.noreply.github.com>
2021-02-20 15:47:49 +0900

    Day2 최지혜 제출

--- 3주차-트리/Day2/Day2_15900_나무탈출_최지혜2.java
+++ 3주차-트리/Day2/Day2_15900_나무탈출_최지혜2.java
@@ -1,48 +1,49 @@
-/*트리순회*/
+/*나무탈출*/
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.StringTokenizer;
 
-public class Boj_15900 {
-	static String YesOrNo = "";
+public class Boj_15900_2 {
 	static int N, cnt=0;
 	static ArrayList<ArrayList<Integer>> edges;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		
+	static boolean[] isVisited;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		String str;
+		N = Integer.parseInt(br.readLine());
+		isVisited = new boolean[N];
+		StringTokenizer st;
 		edges = new ArrayList<>();
-		for(int i=0;i<N+1;i++) {
+		for(int i=0;i<N;i++) {
 			edges.add(new ArrayList<>());
 		}
 		for(int n=0;n<N-1;n++) {
-			str = br.readLine();
-			int i = str.charAt(0)-'0';
-			int j = str.charAt(2)-'0';
-			edges.get(i).add(j);
-			edges.get(j).add(i);
+			st = new StringTokenizer(br.readLine(), " ");
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			edges.get(i-1).add(j);
+			edges.get(j-1).add(i);
 		}
-		escape(1);
-//		System.out.println("cnt : "+ +cnt);
+		escape(0,0);//루트노드인 1번에서 시작
+//		System.out.println("cnt : "+cnt);
 		if(cnt%2 == 1) System.out.println("Yes");
 		else System.out.println("No");
 	}
-	private static void escape(int current) {
-		if(edges.get(current).isEmpty()) {
-			cnt++;
-//			System.out.println(current+" "+ cnt);
+	private static void escape(int current, int level) {
+//		System.out.println("node"+(current+1));
+		if(current!=0 && edges.get(current).size()==1) {
+			cnt+=level;
 			return;
 		}
-//		System.out.println(current+" "+ cnt);
+		isVisited[current] = true;
 		Iterator<Integer> it = edges.get(current).iterator();
 		while(it.hasNext()) {
 			int num = it.next();
-			edges.get(num).remove(Integer.valueOf(current));
-			cnt += edges.get(num).size();
-			escape(num);
+			if(isVisited[num-1]) continue;
+			escape(num-1, level+1);
 		}
 	}
 }
