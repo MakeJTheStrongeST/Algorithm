e89c3ab025a06ec88bca9f4c432df04593cf58be
Jihye Choi <77835382+hotpineapple@users.noreply.github.com>
2021-03-11 16:42:11 +0900

    Day2 최지혜 제출

--- 7주차-다익스트라, 플로이드워셜/Day2/Day2_BOJ1613_최지혜.java
+++ 7주차-다익스트라, 플로이드워셜/Day2/Day2_BOJ1613_최지혜.java
@@ -14,6 +14,7 @@ public class Day2_Boj1613_최지혜 {
 		N = Integer.parseInt(st.nextToken()); // 사건의 개수
 		K = Integer.parseInt(st.nextToken()); // 전후관계의 개수
 		dist = new int[N+1][N+1];
+		
 		for (int i = 0; i < K; i++) {
 			st = new StringTokenizer(br.readLine()," ");
 			dist[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = 1;
